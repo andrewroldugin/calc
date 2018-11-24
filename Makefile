85a08b744ee2de3e992792bdd61103f25833ba98
andrew.roldugin <andrew.roldugin@gmail.com>
2018-11-24 13:54:50 +0300

    Calc grammar & tests

--- Makefile
+++ Makefile
@@ -7,6 +7,7 @@ TESTS = $(PRJ)_tests
 CP = cp $< $@
 CXXFLAGS = -Og -Wextra -Wall -Isrc -std=c++1z
 LDLIBS = -lstdc++
+OBJ = src/calc/grammar.o
 
 all: $(PRJ) $(TESTS)
 
@@ -17,10 +18,14 @@ $(TESTS): src/main_tests
 	$(CP)
 
 src/main_tests: LDLIBS += -lgtest
-src/main_tests: src/main_tests.o src/peg/grammar_test.o
+src/main_tests: src/main_tests.o src/peg/grammar_test.o src/calc/grammar_test.o\
+$(OBJ)
 
 src/peg/grammar_test.o: src/peg/grammar_test.cc src/peg/grammar.h \
 src/peg/parser.h src/peg/ast.h
+
+src/calc/grammar_test.o: src/calc/grammar_test.cc src/calc/grammar.h
+src/calc/grammar.o: src/calc/grammar.cc src/calc/grammar.h
 
 test: $(TESTS)
 	@./$<
---
+++ src/calc/grammar.cc
@@ -0,0 +1,41 @@
+#include "calc/grammar.h"
+
+#include <iostream>
+#include <string>
+
+void calc::PrintNode(TextParser::Node* node, int offset) {
+  while (node) {
+    PrintNode(node->child(),
+              offset + node->end() - node->begin() + 2);
+    std::cout << std::string(offset, '.')
+              << std::string(node->begin(), node->end())
+              << "."
+              << std::endl
+              << std::string(offset, '.') << node->GetRuleTypeInfo().name()
+              << std::endl;
+    node = node->next();
+  }
+}
+
+int calc::EvalNode(TextParser::Node* node) {
+  if (!node) return 0;
+  int out = 0;
+  auto operand = node->child();
+  if (!operand) out = std::stoi(std::string(node->begin(), node->end()));
+  else {
+    out = EvalNode(operand);
+    while (auto op = operand->next()) {
+      operand = op->next();
+      if (op->TypeMatches<calc::Add>()) {
+        out += EvalNode(operand);
+      } else if (op->TypeMatches<calc::Sub>()) {
+        out -= EvalNode(operand);
+      } else if (op->TypeMatches<calc::Mul>()) {
+        out *= EvalNode(operand);
+      } else {
+        out /= EvalNode(operand);
+      }
+    }
+  }
+  return out;
+}
